748d663ee10e3e2324f00cf999087aed0b84b05b
zero <272880214@qq.com>
2021-02-03 23:23:46 +0800

    reference  service配置

--- service-provider/src/main/java/com/lagou/service/HelloServiceImpl.java
+++ service-provider/src/main/java/com/lagou/service/HelloServiceImpl.java
@@ -1,8 +1,15 @@
 package com.lagou.service;
 
+import java.util.concurrent.TimeUnit;
+
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "hello "+name;
     }
 }
